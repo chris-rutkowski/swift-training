170e9bf9853270a0ac48904beaddd96c980cdb41
chris-rutkowski <chrisrutkowski89@gmail.com>
2021-11-05 11:53:24 +0700

    starts learn by doing

---
+++ Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Chris on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
