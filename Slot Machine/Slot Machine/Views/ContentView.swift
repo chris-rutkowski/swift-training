43c35e8e4e3207b4922598383491329651eef780
chris-rutkowski <chrisrutkowski89@gmail.com>
2021-11-06 13:18:17 +0700

    starts slot machine

---
+++ Slot Machine/Slot Machine/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Chris on 06/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
